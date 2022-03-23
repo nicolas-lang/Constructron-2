732c903fef5d9ad550fa5c26cee98e2b73cce389
Nico <git@nicolas-lang.de>
2022-03-23 22:30:49 +0100

    work on job queue

--- script/objects/Surface-manager.lua
+++ script/objects/Surface-manager.lua
@@ -2,6 +2,7 @@ local custom_lib = require("__Constructron-2__.data.lib.custom_lib")
 local Task_construction = require("__Constructron-2__.script.objects.Task-construction")
 local Task_delivery = require("__Constructron-2__.script.objects.Task-delivery")
 local Debug = require("__Constructron-2__.script.objects.Debug")
+local Job = require("__Constructron-2__.script.objects.Job")
 
 -- class Type Surface_manager, nil members exist just to describe fields
 local Surface_manager = {
@@ -48,6 +49,9 @@ function Surface_manager:new(surface, force)
         self.chunks = global.surface_managers[self.id].chunks
         self.tasks = global.surface_managers[self.id].tasks
         self.jobs = global.surface_managers[self.id].jobs
+        for key, action in pairs(Job.actions) do
+            self.job_actions[key] = action(self)
+        end
     end
 end
 
@@ -64,6 +68,31 @@ end
 function Surface_manager:destroy()
     self:log()
     global.surface_managers[self.id] = nil
+end
+-------------------------------------------------------------------------------
+--  Surface Processing
+-------------------------------------------------------------------------------
+
+function Surface_manager:update_units(limit) -- luacheck: ignore
+end
+
+function Surface_manager:get_free_constructron() -- luacheck: ignore
+    return nil
+end
+
+-------------------------------------------------------------------------------
+--  Entity Processing
+-------------------------------------------------------------------------------
+function Surface_manager:process_entity(entity)
+    self:log()
+    if
+        entity and entity.valid and
+            (entity.type == "entity-ghost" or entity.type == "tile-ghost" or entity.to_be_upgraded() or entity.to_be_deconstructed() or entity.get_health_ratio() < 0.95 or
+                entity.type == "item-request-proxy" or
+                entity.name == "ctron-buffer-chest")
+     then
+        self:register_entity(entity)
+    end
 end
 
 function Surface_manager:register_entity(entity)
@@ -82,7 +111,7 @@ function Surface_manager:register_entity(entity)
     end
 end
 
-function Surface_manager:unregister_entity(entity)-- luacheck: ignore
+function Surface_manager:unregister_entity(entity) -- luacheck: ignore
     self:log()
     local x, y = Surface_manager.chunk_from_position(entity.position)
     local key = x .. "/" .. y
@@ -91,36 +120,6 @@ function Surface_manager:unregister_entity(entity)-- luacheck: ignore
     if custom_lib.table_length(self.chunks[key]) == 0 then
         self.chunks[key] = nil
     end
-end
-
-function Surface_manager:run_jobs(limit)-- luacheck: ignore
-    self:log()
-    limit = limit or 10
-    local key, chunk = next({})
-    local c = 0
-    while key and c < limit do
-    -- loop through jobs
-    --      run each job's action driver & state transition
-    end
-    return c
-end
-
-
-function Surface_manager:assign_jobs(limit)
-    self:log()
-    limit = limit or 10
-    local key, chunk = next({})
-    local c = 0
-    while key and c < limit do
-        --  select 1st free constructron
-        --      get next task
-        --           if job already has tasks: sort by mean distance
-        --      assign task
-        --          split task if to large
-        --          insert task with remainder to front of task-queue if task was split
-        --      get next (until >80% full or next task fits in full or distance > 1.5 chunks)
-    end
-    return c
 end
 
 function Surface_manager:assign_tasks(limit)
@@ -164,16 +163,55 @@ function Surface_manager:assign_tasks(limit)
     return c
 end
 
-function Surface_manager:process_entity(entity)
+-------------------------------------------------------------------------------
+--  Job Processing
+-------------------------------------------------------------------------------
+function Surface_manager:run_jobs()
     self:log()
-    if
-        entity and entity.valid and
-            (entity.type == "entity-ghost" or entity.type == "tile-ghost" or entity.to_be_upgraded() or entity.to_be_deconstructed() or entity.get_health_ratio() < 0.95 or
-                entity.type == "item-request-proxy" or
-                entity.name == "ctron-buffer-chest")
-     then
-        self:register_entity(entity)
+    for key, job in pairs(self.jobs) do
+        -- each job state has a state-named action class which executes a job based action and returnes a state transition
+        -- the state action is expected to work only on surfacemanager and job objects and their members
+        -- The surfacemanager provides stations and surface related information, the job has task and ctron childs
+        local state_based_action = self.job_actions[job:get_state()]
+        local new_state = state_based_action.handleStateTransition(job)
+        if new_state then
+            job:set_state()
+        end
+        game.print(job:get_state())
+        if job:get_state() == Job.state.job_completed then
+            job:destroy()
+            self.jobs[key] = nil
+        end
     end
+end
+
+function Surface_manager:assign_jobs(limit)
+    self:log()
+    limit = limit or 10
+    local c = 0
+    local key, task = next(self.tasks)
+    local unit = self:get_free_constructron()
+    while task and unit and c < limit do
+        --  select 1st free constructron
+        local unit = self:get_free_constructron()
+        local job = Job()
+        --just simple 1:1  - fix later
+        key, task = next(self.tasks)
+        job.add_task(task)
+        self.tasks[key] = nil
+        self.jobs[#(self.jobs) + 1] = job
+        --ToDo
+        --      get next task
+        --           if job already has tasks: sort by mean distance
+        --      assign task
+        --          split task if to large
+        --          insert task with remainder to front of task-queue if task was split
+        --      get next (until >80% full or next task fits in full or distance > 1.5 chunks)
+        has_task = next(self.tasks)
+        local unit = self:get_free_constructron()
+        c = c + 1
+    end
+    return c
 end
 
 return Surface_manager
