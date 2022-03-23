1e8e9188529fe468c6e0713feb61515d2af1b317
Nico <git@nicolas-lang.de>
2022-03-23 22:29:08 +0100

    misc fixes

--- script/simple_movement_controller.lua
+++ script/simple_movement_controller.lua
@@ -16,32 +16,44 @@ me.main = function(event)
     if event.tick % 120 == 0 then
         log("tick")
         local p1 = {
-            x = -22,
-            y = 15
+            x = -96,
+            y = 0
         }
         local p2 = {
-            x = 22,
-            y = 15
+            x = 96,
+            y = 0
         }
         local p3 = {
-            x = 0,
-            y = 25
+            x = 64,
+            y = -64
+        }
+        local p4 = {
+            x = 32,
+            y = -64
+        }
+        local p5 = {
+            x = 33,
+            y = -63
+        }
+        local p6 = {
+            x = 33,
+            y = -65
         }
         local target
-        local target_distance = 2.5
+        local target_distance = 5
         for unit_number, unit in pairs(me.ctrons) do
             log("unit_number" .. unit_number)
             if unit:is_valid() then
                 log("status:" .. (unit:get_status_name()))
                 if unit:get_status_name() == "moving" then
                     log("Ctron.status.moving")
-                    if unit:distance_to(p1) < 3 then
+                    if unit:distance_to(p1) < target_distance then
                         log("at p1 " .. unit_number)
                         request = {}
                         request["copper-plate"] = 10
                         unit:set_request_items(request)
                         unit:set_status("requesting")
-                    elseif unit:distance_to(p2) < 3 then
+                    elseif unit:distance_to(p2) < target_distance then
                         log("at p2 " .. unit_number)
                         request = {}
                         request["iron-plate"] = 10
@@ -68,16 +80,30 @@ me.main = function(event)
                     log("Ctron.status.idle")
                     if unit:distance_to(p1) < target_distance then
                         target = p2
+                    elseif unit:distance_to(p1) < 2 * target_distance then
+                        target = p1
                     elseif unit:distance_to(p2) < target_distance then
                         target = p3
+                    elseif unit:distance_to(p2) < 2 * target_distance then
+                        target = p2
                     elseif unit:distance_to(p3) < target_distance then
-                        target = p1
+                        unit:set_autopilot(
+                            {
+                                {position = p3},
+                                {position = p4},
+                                {position = p5},
+                                {position = p6},
+                                {position = p1}
+                            }
+                        )
+                    elseif unit:distance_to(p3) < 2 * target_distance then
+                        target = p3
                     else
                         target = p1
                         game.print("reset " .. unit_number)
                     end
 
-                    if not unit:is_moving() then
+                    if not unit:is_moving() and target then
                         log("move " .. unit_number)
                         unit:go_to(target)
                     end
